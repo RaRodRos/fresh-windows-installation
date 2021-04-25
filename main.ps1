0202997794bbd4feeec35d4739aa3f9370e11693
Ra! <62754237+RaRodRos@users.noreply.github.com>
2021-04-25 18:46:57 +0200

    Execution of the script without invoke-expression

--- main.ps1
+++ main.ps1
@@ -1,1 +1,2 @@
-iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/JOPU5'))
+$script = [Scriptblock]::Create((New-Object System.Net.WebClient).DownloadString('https://git.io/JOPU5'))
+Start-Process powershell.exe -ArgumentList "-ExecutionPolicy Bypass -NoExit -Command $script" -Verb RunAs
