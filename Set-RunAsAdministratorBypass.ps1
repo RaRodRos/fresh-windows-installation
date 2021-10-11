be875d4352e4b86bcf7004819ea8cfd666e9b6f9
Ra! <62754237+RaRodRos@users.noreply.github.com>
2021-10-11 19:08:10 +0200

    Add script for RunAsAdministratorBypass

---
+++ Set-RunAsAdministratorBypass.ps1
@@ -0,0 +1,28 @@
+# Add an entry to the context menu to execute Powershell files as administrator with bypass policy
+# This script do the same as adding RunAsAdministratorBypass.reg to the registry
+
+if (!($psPath = (Get-Process pwsh -ErrorAction 'SilentlyContinue' | Select-Object -First 1).Path)) {
+    $psPath = (Get-Process powershell | Select-Object -First 1).Path
+}
+
+$paramsEntry = @{
+	Path = "Registry::HKLM\software\classes\Microsoft.PowerShellScript.1\Shell"
+	Name = "RunAsAdministratorBypass"
+	Value = "Execute as administrator with bypass policy"
+}
+$entry = New-Item @paramsEntry
+$entry.SetValue('Icon', $psPath)
+
+$commandParams = @{
+	Path = $entry.PSPath
+	Name = "Command"
+	Value = "$psPath -Command `"& {Start-Process $(Split-Path $psPath -leaf) -ArgumentList `'-ExecutionPolicy Bypass -NoExit -File `"%1`"`' -Verb RunAs}`""
+}
+New-Item @commandParams | Out-Null
+
+$positionParams = @{
+	Path = $entry.PSPath
+	Name = "Position"
+	Value = "Top"
+}
+New-Item @positionParams | Out-Null
