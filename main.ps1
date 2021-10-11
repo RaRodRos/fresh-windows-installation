b9e789c2b7d7ca408a84d1c79453908c6179cfcb
Ra! <62754237+RaRodRos@users.noreply.github.com>
2021-10-11 19:33:23 +0200

    Add all scripts to main

--- main.ps1
+++ main.ps1
@@ -1,12 +1,21 @@
-$main = [Scriptblock]::Create((New-Object System.Net.WebClient).DownloadString())
-$setLowUAC = [Scriptblock]::Create((New-Object System.Net.WebClient).DownloadString())
-$installPMSoftware = [Scriptblock]::Create((New-Object System.Net.WebClient).DownloadString())
+$main = (New-Object System.Net.WebClient).DownloadString('https://git.io/Jrjhg')
+# $setLowUAC = (New-Object System.Net.WebClient).DownloadString('https://git.io/JoeeB')
+# $installPMSoftware = (New-Object System.Net.WebClient).DownloadString('https://git.io/Joevt')
+# $setRunAsAdministratorBypass = (New-Object System.Net.WebClient).DownloadString('https://git.io/JoNhe')
+$setLowUAC = (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/RaRodRos/junk/master/print-jarl.ps1')
+$installPMSoftware = (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/RaRodRos/junk/master/print-jarl.ps1')
+$setRunAsAdministratorBypass = (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/RaRodRos/junk/master/print-jarl.ps1')
+
+if (!$main -or !($setLowUAC -Or $installPMSoftware)) {
+	throw "There is no script available"
+}
 
 # Relaunch the script in administrator mode if necessary
-If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
 	Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -NoExit -Command $main" -Verb RunAs
 	Exit
 }
 
-& $setLowUAC
-& $installPMSoftware
+Invoke-Expression -Command $setLowUAC
+Invoke-Expression -Command $setRunAsAdministratorBypass
+Invoke-Expression -Command $installPMSoftware
