524f3e42fe0b84e106f8be207f723636624b3b97
Ra <rarodriguezrosado@gmail.com>
2021-10-20 12:42:26 +0200

    add set-accessibilitykeys.ps1

---
+++ Set-AccessibilityKeys.ps1
@@ -0,0 +1,3 @@
+Set-ItemProperty -Path "HKCU:\Control Panel\Accessibility\StickyKeys" -Name "Flags" -Type String -Value "506"
+Set-ItemProperty -Path "HKCU:\Control Panel\Accessibility\ToggleKeys" -Name "Flags" -Type String -Value "58"
+Set-ItemProperty -Path "HKCU:\Control Panel\Accessibility\Keyboard Response" -Name "Flags" -Type String -Value "122"
